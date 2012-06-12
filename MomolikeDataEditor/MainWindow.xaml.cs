2dfd64a3586f56a3a155217c8ebd94ec177df3d4
Gabriel Wilds <jetsetlemming@gmail.com>
2012-06-12 16:19:40 -0400

    Initial commit. UI and Data/View Model structure created.

---
+++ MomolikeDataEditor/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MomolikeDataEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.DataContext = new MainWindowViewModel();
+            InitializeComponent();
+        }
+
+        private void Browse(object sender, RoutedEventArgs e)
+        {
+            ((MainWindowViewModel)this.DataContext).Browse();
+        }
+
+        private void Load(object sender, RoutedEventArgs e)
+        {
+            ((MainWindowViewModel)this.DataContext).Load();
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            ((MainWindowViewModel)this.DataContext).Save();
+        }
+
+        private void AddAnimation(object sender, RoutedEventArgs e)
+        {
+            ((MainWindowViewModel)this.DataContext).AddAnimation();
+        }
+    }
+}
---
+++ MomolikeDataEditor/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MomolikeDataEditor
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        public List<Animation> Animations
+        {
+            get;
+            set;
+        }
+
+        public void Browse()
+        {
+            //open file browser to point at a sprite sheet, defaulting to game dir
+        }
+
+        public void Load()
+        {
+            //fill canvas with selected sprite sheet
+        }
+
+        public void Save()
+        {
+            //save the animation definitions and data to JSON text file to be read by momolike
+        }
+
+        public void AddAnimation()
+        {
+            Animations.Add(new Animation());
+            //add a new default Animation to the list, to be defined by the user.
+        }
+    }
+}
